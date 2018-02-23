940bcc74bc2727b563dd499495464edd3daf0d3c
huzhengtao <452580164@qq.com>
2018-02-23 16:51:06 +0800

    我的修改了代码

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
+  // 我修改了代码
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
